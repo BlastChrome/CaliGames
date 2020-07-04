8c8484e941a3ca21e40a40feaf414bf44e247415
Nick <nicdogg360@gmail.com>
2020-07-04 11:32:38 -0700

    Created nav-bar + asset folder from dropbox

--- root/js/app.js
+++ root/js/app.js
@@ -1,2 +1,24 @@
-let hamburger = document.getElementById('hamburger'); 
+// Mobile Navbar functionality 
+const navAnim = () => {
+    let hamburger = document.getElementById('hamburger');  
+    let nav = document.querySelector('nav'); 
+    let navLinks = document.querySelectorAll('#nav_links li a');
+    
+    hamburger.addEventListener('click',()=>{
+        nav.classList.toggle('nav_active');
+        
+        //link animation 
+        navLinks.forEach((element, index) => {
+            if(element.style.animation){
+                element.style.animation = '';
+            } else{
+                element.style.animation =  `navLinkFade 1 ease forwards ${index / 7 + 1}s `; 
+                console.log("ease");
+            }
+        });
+    }) 
+} 
+
+navAnim();
+
 
